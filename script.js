04fb8a27b559af06022d8809a51a5da86325f597
Mathanstunnerz <mathanstunnerz95@gmail.com>
2022-12-22 14:51:51 +0530

    call back number task

---
+++ script.js
@@ -0,0 +1,189 @@
+function one(){
+    two();
+    one2()
+    var one =""
+    console.log(one );
+    var arr = document.querySelector(".mathan")
+    arr.innerText = one 
+}
+setTimeout(one,2000)
+function one2(){
+    var one2 ="1"
+    console.log(one2 );
+    var arr = document.querySelector(".mathan")
+    arr.innerText = one2 
+}
+setTimeout(one2,12000)
+function two(){
+    three();
+    two2()
+    var two=""
+    console.log(two );
+    var arr = document.querySelector(".mathan")
+    arr.innerText = two2
+}
+setTimeout(two,2000)
+function two2(){
+    var two2 ="2"
+    console.log(two2 );
+    var arr = document.querySelector(".mathan")
+    arr.innerText = two2 
+}
+setTimeout(two2,11000)
+function three(){
+    four();
+    three2()
+    var three =""
+    console.log(three );
+    var arr = document.querySelector(".mathan")
+    arr.innerText = three 
+
+}
+setTimeout(three,2000)
+function three2(){
+    var three2 ="3"
+    console.log(three2 );
+    var arr = document.querySelector(".mathan")
+    arr.innerText = three2 
+}
+setTimeout(three2,10000)
+function four(){
+    five();
+    four2()
+    var four =""
+    console.log(four);
+    var arr = document.querySelector(".mathan")
+    arr.innerText = four
+  
+}
+setTimeout(four,2000)
+function four2(){
+    var four2 ="4"
+    console.log(four2);
+    var arr = document.querySelector(".mathan")
+    arr.innerText = four2
+}
+setTimeout(four2,9000)
+function five(){
+    six();
+    five2()
+    var five =""
+    console.log(five);
+    var arr = document.querySelector(".mathan")
+    arr.innerText = five
+}
+setTimeout(five,2000)
+function five2(){
+    var five2 ="5"
+    console.log(five2);
+    var arr = document.querySelector(".mathan")
+    arr.innerText = five2
+}
+setTimeout(five2,8000)
+function six(){
+    sevan();
+   six2()
+   var six =""
+   console.log(six);
+   var arr = document.querySelector(".mathan")
+   arr.innerText = six
+}
+setTimeout(six,2000)
+function six2(){
+    var six2 ="6"
+    console.log(six2);
+    var arr = document.querySelector(".mathan")
+    arr.innerText = six2
+}
+setTimeout(six2,7000)
+function sevan(){
+    eight();
+    sevan_2()
+    var sevan =""
+    console.log(sevan);
+    var arr = document.querySelector(".mathan")
+    arr.innerText = sevan
+   
+}
+setTimeout(sevan,2000)
+function sevan_2(){
+    var sevan2 ="7"
+    console.log(sevan2);
+    var arr = document.querySelector(".mathan")
+    arr.innerText = sevan2
+}
+setTimeout(sevan_2,6000)
+function eight(){
+    nine();
+    eight_2()
+    var eight_1 =""
+    console.log(eight_1);
+    var arr = document.querySelector(".mathan");
+    arr.innerText = eight_1
+}
+setTimeout(eight,2000)
+function eight_2(){
+    var eight2 ="8"
+    console.log(eight2);
+    var arr = document.querySelector(".mathan")
+    arr.innerText = eight2
+}
+setTimeout(eight_2,5000)
+function nine(){
+    ten();
+    nine_1()
+    var nine1 =""
+    console.log(nine1);
+    var arr = document.querySelector(".mathan")
+    arr.innerText = nine1
+}
+setTimeout(nine,2000)
+function nine_1(){
+    var nine = "9"
+    console.log(nine);
+    var arr = document.querySelector(".mathan")
+    arr.innerText = nine
+}
+setTimeout(nine_1,4000)
+function ten(){
+      ten11()
+    var onety ="10"
+    console.log(onety);
+    var arr = document.querySelector(".mathan")
+    arr.innerText = onety
+  
+}
+setTimeout(ten,2000)
+function ten11(){
+    var one = "10"
+    console.log(one);
+    var arr = document.querySelector(".mathan")
+    arr.innerText = one
+}
+setTimeout(ten11,3000)
+function independant(){
+    nem_3()
+    var nem =""
+    var arr = document.querySelector(".mathan")
+    arr.innerText = nem   
+
+}
+setTimeout(independant,2000)
+function nem_3(){
+
+    var nem_3 = "🧡🌼💚Happy Independence Day🧡🌼💚"
+    console.log(nem_3)
+ var arr = document.querySelector(".mathan")
+ arr.innerText = nem_3
+    }
+
+setTimeout(nem_3,13000)
+
+var body =document.querySelector(".inde")
+var demo = document.querySelector(".inde")
+var first_ = document.createElement("div")
+first_.classList.add("background")
+ body.append(first_)
+ var fi_ = document.createElement("p")
+ fi_.classList.add("mathan")
+ first_.append(fi_)
